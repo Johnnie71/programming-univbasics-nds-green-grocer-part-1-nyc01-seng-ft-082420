572052bf1e5b607e58624e426ca920a4b84b153c
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-19 21:07:08 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -18,7 +18,8 @@ def consolidate_cart(cart)
      new_cart_item[:count] += 1 
    else
      new_cart_item = {
-       :item => 
+       :item => cart[counter][:item]
+       
      }
    end
    counter += 1 
